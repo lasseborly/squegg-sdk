888b2cf9900172b97f8d9280c4b490ba1d468184
Lasse Borly <lasseborly@gmail.com>
2021-03-14 22:25:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Squegg SDK
+
+```sh
+npm install squegg-sdk
+```
+
+```javascript
+import { parseSquegg } from "squegg-sdk"
+
+/* 
+ * Character codes should come from the device itself and it's your job to
+ * convert them into an array of numbers.
+ * See codesandbox example for a web implementation.
+ */
+const liveCharCodes = [49]
+
+```
---
+++ src/index.ts
@@ -0,0 +1,57 @@
+export type CharCodes = number[];
+
+export interface SqueggData {
+  strength: number;
+  isSqueezing: boolean;
+  batteryCharge: number;
+}
+
+export const SERVICE_UUID = "0000ffb0-0000-1000-8000-00805f9b34fb";
+export const CHARACTERISTIC_UUID = "0000ffb2-0000-1000-8000-00805f9b34fb";
+export const DEVICE_NAME = "weixin-nini";
+
+export const BLUETOOTH_DEVICE_OPTIONS = {
+    filters: [{ name: DEVICE_NAME }, { services: [SERVICE_UUID] }]
+};
+
+export type Percentage = number;
+
+export function _parsebatteryCharge(raw: string): Percentage {
+  const AVOID_ZERO = 1;
+  const PERCENTAGE_MULTIPLIER = 10;
+  return PERCENTAGE_MULTIPLIER * (Number(raw) + AVOID_ZERO);
+}
+
+export function _parseSqueezing(raw: string): boolean {
+  return Boolean(Number(raw));
+}
+
+export function _parseStrength(values: string[]): number {
+  const raw = values.join("");
+  return Number(parseFloat(raw).toFixed(1));
+}
+
+export function _fromCharCode(charCodes: CharCodes): string[] {
+  return charCodes.map((code) => String.fromCharCode(code));
+}
+
+export function dataViewToArray(dataView: DataView): number[] {
+  return Array.from(new Int8Array(dataView.buffer));
+}
+
+export function parseSquegg(charCodes: CharCodes): SqueggData {
+  const values: string[] = _fromCharCode(charCodes);
+  // The first values is not a known representation.
+  values.shift();
+
+  const rawBatteryCharge: string = values.shift() || "0";
+  const rawSqueezing: string = values.shift() || "0";
+
+  return {
+    strength: _parseStrength(values),
+    isSqueezing: _parseSqueezing(rawSqueezing),
+    batteryCharge: _parsebatteryCharge(rawBatteryCharge)
+  };
+}
+
+export default parseSquegg;
